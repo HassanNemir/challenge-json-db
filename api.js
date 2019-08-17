4350060c614fbe32fff1eccf8b08dc55d7259fe3
HassanNemir <hassan@hnemir.me>
2019-08-18 01:08:51 +0200

    feat: store data endpoints and handlers
    write data to json file in ./data folder, and edit available data
    added utility functions to access and check for nested properties in the stored object

--- api.js
+++ api.js
@@ -1,5 +1,13 @@
 module.exports = {
-  getHealth
+  storeStudentData,
+}
+
+function storeStudentData (req, res, next) {
+  const studentParams = util.getStudentParams(req.params, req.body)
+  const { studentId, studentProporties, value } = studentParams
+  const student = util.handleStudentData(studentId, studentProporties, value)
+  res.json(student)
+}
 }
 
 async function getHealth (req, res, next) {
---
+++ util.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+
+module.exports = {
+  handleStudentData,
+  checkStudentDataAvailability,
+  getStudentParams
+}
+
+function handleStudentData (id, keys, value) {
+  let avKeys = checkStudentDataAvailability(id, keys)
+  if (avKeys) {
+    const student = requireUncached(`./data/${id}`)
+    const arr = keys.slice()
+    arr.splice(arr.indexOf(avKeys[0]))
+    let stKey = getNestedObject(student, arr)
+    assignNestedKeys(stKey, avKeys, value)
+    fs.writeFileSync(`./data/${id}.json`, JSON.stringify(student, null, 2))
+    return student
+  }
+  const student = {}
+  assignNestedKeys(student, keys, value)
+  fs.writeFileSync(`./data/${id}.json`, JSON.stringify(student, null, 2))
+  return student
+}
+
+function requireUncached (module) {
+  delete require.cache[require.resolve(module)]
+  return require(module)
+}
+
+function getStudentParams (params, body) {
+  const { studentId } = params
+  const studentProporties = params[0].split(/\//)
+  Object.keys(body)[0] && studentProporties.push(Object.keys(body)[0])
+  const value = body[Object.keys(body)[0]]
+  return { studentId, studentProporties, value }
+}
+
+function getNestedObject (nestedObj, pathArr) {
+  return pathArr.reduce(
+    (obj, key) => (obj && obj[key] !== 'undefined' ? obj[key] : undefined),
+    nestedObj
+  )
+}
+
+function checkStudentDataAvailability (id, keys) {
+  try {
+    const student = requireUncached(`./data/${id}.json`)
+    const arr = keys.slice()
+    return arr.reduce((obj, key, i) => {
+      if (obj && key in obj && i !== arr.length - 1) {
+        return obj[key]
+      }
+      return arr.splice(i)
+    }, student)
+  } catch (error) {
+    return false
+  }
+}
+
+function assignNestedKeys (obj, keys, value) {
+  const lastKeyIndex = keys.length - 1
+  keys.map((key, i) => {
+    if (i !== lastKeyIndex) {
+      obj[key] = {}
+      obj = obj[key]
+    }
+  })
+  obj[keys[lastKeyIndex]] = value || {}
+}
