1fe80324bd35be6e8c69052ccdbc6cf3a9036dc4
HassanNemir <hassan@hnemir.me>
2019-08-18 01:20:59 +0200

    test: Endpoints testing
    Added test suites for storing, getting and deleting objects, and testing the same for nested properties.

--- test.js
+++ test.js
@@ -1,21 +1,91 @@
 const tape = require('tape')
 const jsonist = require('jsonist')
+const fs = require('fs')
 
 const port = (process.env.PORT = process.env.PORT || require('get-port-sync')())
 const endpoint = `http://localhost:${port}`
 
 const server = require('./server')
 
-tape('health', async function (t) {
-  const url = `${endpoint}/health`
+tape('Store Data', async function (t) {
+  const data = { firstName: 'Joe' }
+  const url = `${endpoint}/s10/name`
+  jsonist.put(url, data, (err, body) => {
+    if (err) t.error(err)
+    t.equal(JSON.stringify(body), '{"name":{"firstName":"Joe"}}')
+    t.end()
+  })
+})
+
+tape('Edit Nested Data', async function (t) {
+  const data = { lastName: 'Doe' }
+  const url = `${endpoint}/s10/name`
+  jsonist.put(url, data, (err, body) => {
+    if (err) t.error(err)
+    t.equal(
+      JSON.stringify(body),
+      '{"name":{"firstName":"Joe","lastName":"Doe"}}'
+    )
+    t.end()
+  })
+})
+
+tape('Get Data', async function (t) {
+  const url = `${endpoint}/s10/name`
   jsonist.get(url, (err, body) => {
     if (err) t.error(err)
-    t.ok(body.success, 'should have successful healthcheck')
+    t.equal(JSON.stringify(body), '{"firstName":"Joe","lastName":"Doe"}')
+    t.end()
+  })
+})
+
+tape('Get Nested Data', async function (t) {
+  const url = `${endpoint}/s10/name/firstName`
+  jsonist.get(url, (err, body) => {
+    if (err) t.error(err)
+    t.equal(JSON.stringify(body), '"Joe"')
+    t.end()
+  })
+})
+
+tape('Get Nested Data Not Found', async function (t) {
+  const url = `${endpoint}/s10/name/firstName/hello`
+  jsonist.get(url, (err, body) => {
+    if (err) t.error(err)
+    t.equal(JSON.stringify(body), '{"error":"Not Found"}')
+    t.end()
+  })
+})
+
+tape('Delete Nested Data', async function (t) {
+  const url = `${endpoint}/s10/name/firstName`
+  jsonist.delete(url, (err, body) => {
+    if (err) t.error(err)
+    t.equal(JSON.stringify(body), '{"name":{"lastName":"Doe"}}')
+    t.end()
+  })
+})
+
+tape('Delete Nested Data Not Found', async function (t) {
+  const url = `${endpoint}/s10/name/firstName/hello`
+  jsonist.delete(url, (err, body) => {
+    if (err) t.error(err)
+    t.equal(JSON.stringify(body), '{"error":"Not Found"}')
+    t.end()
+  })
+})
+
+tape('Delete Data', async function (t) {
+  const url = `${endpoint}/s10/name`
+  jsonist.delete(url, (err, body) => {
+    if (err) t.error(err)
+    t.equal(JSON.stringify(body), '{}')
     t.end()
   })
 })
 
 tape('cleanup', function (t) {
+  fs.unlinkSync('./data/s10.json')
   server.close()
   t.end()
 })
